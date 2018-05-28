081732a2cd69e772216a2b1f4b025b34b1ddc186
arononeill <arononeill98@gmail.com>
2018-05-28 23:53:55 +0100

    Tuple Basics

--- Variable_Practice/Tuple_Usability.py
+++ Variable_Practice/Tuple_Usability.py
@@ -8,3 +8,14 @@ print "\n Print an item from a certain position in the tuple\n"
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[1])
 
+print "\nThe len() method returns the number of items in a tuple\n\nThe number of items in this tuple are: "
+print(len(thistuple))
+
+print "\nUsing the tuple() method to make a tuple\n\nIt's contents are :"
+thistuple2 = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple2)
+
+print "\n Attempt at changing variable located at position 1, but you cannot change values in a tuple\n"
+thistuple = ("apple", "banana", "cherry")
+thistuple[1] = "blackcurrant" # test changeability
+print(thistuple)
