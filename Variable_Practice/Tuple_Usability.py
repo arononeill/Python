268f26e9d87975e5ee2eed94bcafccc027920c49
arononeill <arononeill98@gmail.com>
2018-05-28 23:41:55 +0100

    Tuple Basics

---
+++ Variable_Practice/Tuple_Usability.py
@@ -0,0 +1,9 @@
+# Create a Tuple Variable
+thistuple = ("apple", "banana", "cherry")
+
+# Displaying it
+print(thistuple)
+
+# Print an item from a certain position in the tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
