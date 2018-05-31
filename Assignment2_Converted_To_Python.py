3cc4efce5480e0c7f07987944a46993ed8ce0136
arononeill <arononeill98@gmail.com>
2018-05-31 20:13:06 +0100

    Converting Assignment2.c To Python

--- Assignment2_Converted_To_Python.py
+++ Assignment2_Converted_To_Python.py
@@ -4,13 +4,17 @@ option = 0
 correctly_entered = 0
 Incorrectly_entered = 0   
 EnterPinFirst = 0  
-EncryptFirst = 0  
+EncryptFirst = 0 
+EncryptTwice = 0 
+DecryptTwice = 0 
 pin = [0, 0, 0, 0]
 
 def PIN():
     "Enters the user's pin"
     print "Enter your 4 digit PIN one digit at a time\n"
     PinNum = 0
+    EncryptTwice = 0
+    DecryptTwice = 0
 
     while PinNum < 4:
         num = input()
@@ -30,28 +34,35 @@ def PIN():
 
 def ENCRYPT(EnterPinFirst):
     "Encrypts the user's pin"
+    global EncryptFirst
     if EnterPinFirst == 1:
-        # Algorithm to encrypt the user's pin
-        temp = pin[0]
-        pin[0] = pin[2]
-        pin[2] = temp
+        global EncryptTwice
+        if EncryptTwice == 0:
+            # Algorithm to encrypt the user's pin
+            temp = pin[0]
+            pin[0] = pin[2]
+            pin[2] = temp
 
-        temp = pin[1]
-        pin[1] = pin[3]
-        pin[3] = temp
+            temp = pin[1]
+            pin[1] = pin[3]
+            pin[3] = temp
 
-        PinNum = 0
-        while PinNum < 4:
-            pin[PinNum] += 1
+            PinNum = 0
+            while PinNum < 4:
+                pin[PinNum] += 1
 
-            if pin[PinNum] == 10:
-                pin[PinNum] = 0
+                if pin[PinNum] == 10:
+                    pin[PinNum] = 0
 
-            PinNum += 1
+                PinNum += 1
 
-        print "The pin you encrypted pin is ", pin
-        EncryptFirst = 1
-        COMPARE();
+            print "The pin you encrypted pin is ", pin
+            EncryptFirst = 1
+            EncryptTwice = 1
+            COMPARE();
+
+        else:
+            print "\n\t*** Sorry you cannot encrypt the same pin twice ***\n"
 
     else:
         print "\n\t*** Please Enter a pin First ***\n"
@@ -83,24 +94,30 @@ def DECRYPT(EnterPinFirst, EncryptFirst):
 
     if EnterPinFirst == 1:
         if EncryptFirst == 1:
-            temp = pin[3]
-            pin[3] = pin[1]
-            pin[1] = temp
+            global DecryptTwice
+            if DecryptTwice == 0:
+                temp = pin[3]
+                pin[3] = pin[1]
+                pin[1] = temp
 
-            temp = pin[0]
-            pin[0] = pin[2]
-            pin[2] = temp
+                temp = pin[0]
+                pin[0] = pin[2]
+                pin[2] = temp
 
-            PinNum = 0
-            while PinNum < 4:
-                pin[PinNum] -= 1
+                PinNum = 0
+                while PinNum < 4:
+                    pin[PinNum] -= 1
 
-                if pin[PinNum] == -1:
-                    pin[PinNum] = 9
+                    if pin[PinNum] == -1:
+                        pin[PinNum] = 9
 
-                PinNum += 1
+                    PinNum += 1
 
-            print "Your newly decrypted pin is ", pin 
+                print "Your newly decrypted pin is ", pin 
+                DecryptTwice = 1
+
+            else:
+                print "\n\t*** Sorry you cannot decrypt the same pin twice ***\n"
 
         else:
             print "\n\t***Please Encrypt your pin first ***\n"
