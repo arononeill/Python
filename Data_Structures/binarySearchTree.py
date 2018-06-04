15e626accd65dcea3c05f08e9816ade55ec20791
arononeill <arononeill98@gmail.com>
2018-06-04 22:07:58 +0100

    Implementation of a Binary Search Tree

--- Data_Structures/binarySearchTree.py
+++ Data_Structures/binarySearchTree.py
@@ -1,10 +1,10 @@
 class Node:
     # Tree node: left and right child + data which can be any object
 
+    # This creates a new class named Node with 3 attributes, left node, right node and node's data
     def __init__(self, data):
 
         # Node constructor
-
         self.left = None
         self.right = None
         self.data = data
@@ -12,17 +12,17 @@ class Node:
     def insert(self, data):
 
         """ Insert new node with data
-
         @param data node data object to insert """
 
         if self.data:
 
-            if data < self.data:
+            if data < self.data: # Checks whether the node to insert is less than the root node
 
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
+                if self.left is None: # When the left child is none 
+                    self.left = Node(data) # We attach the value to insert into it
+
+                else: # If there is a left sub node then this else statement is called
+                    self.left.insert(data) # The insert() method is called again, now with the subtree's value, along with the same value for data
 
             elif data > self.data:
 
@@ -41,21 +41,22 @@ class Node:
         @param parent node's parent
         @returns node and node's parent if found or None, None """
 
-        if data < self.data:
+        if data < self.data: # Checks whether the value given to lookup is less than the root's data
 
             if self.left is None:
-                return None, None
-            return self.left.lookup(data, self)
+                return None, None # Value not found, none returned
 
-        elif data > self.data:
+            return self.left.lookup(data, self) # root's left child's lookup() method is called using the same data whilst making the parent value equal to the current node
+
+        elif data > self.data: # If the value searched is greater than the node's value
 
             if self.right is None:
-                return None, None
+                return None, None # Value not found, none returned
 
-            return self.right.lookup(data, self)
+            return self.right.lookup(data, self) # root's right child's lookup() method is called using the same data whilst making the parent value equal to the current node
 
-        else:
-            return self, parent
+        else: # When the node's data is equal to the value searched 
+            return self, parent # We return that value as well as it's parent's node value
 
     def delete(self, data):
 
@@ -66,9 +67,9 @@ class Node:
         node, parent = self.lookup(data)
 
         if node is not None:
-            children_count = node.children_count()
+            children_count = node.children_count() # Gets the number of children and returns it, assigning it to children_count
 
-        if children_count == 0:
+        if children_count == 0: 
             # if node has no children, just remove it
             if parent:
                 if parent.left is node:
@@ -82,17 +83,23 @@ class Node:
         elif children_count == 1:
             # if node has 1 child
             # replace node with its child
-            if node.left:
-                n = node.left
+
+            if node.left: 
+                n = node.left # If the node has a left child, it is assigned to n
             else:
-                n = node.right
-            if parent:
-                if parent.left is node:
-                    parent.left = n
-                else:
-                    parent.right = n
+                n = node.right # If the node has a right child, it is assigned to n
+
+            if parent: 
+                if parent.left is node: # If the left child of the parent is the value of the searched for node
+                    parent.left = n # The left child of the parent is assigned the value of n, which is the value of the child, of the parent's child
+                                    # This swpas the parent's child and child's child
+
+                else: # If the right child of the parent is the value of the searched for node
+                    parent.right = n # The right child of the parent is assigned the value of n, which is the value of the child, of the parent's child
+                                     # This swpas the parent's child and child's child
                 del node
-            else:
+
+            else: # This handles the case where the node is the root of the tree
                 self.left = n.left
                 self.right = n.right
                 self.data = n.data
@@ -100,22 +107,25 @@ class Node:
         else:
             # if node has 2 children
             # find its successor
-            parent = node
-            successor = node.right
-            while successor.left:
-                parent = successor
-                successor = successor.left
-            # replace node data by its successor data
+            parent = node # Node searched to be deleted is assigned to parent
+            successor = node.right # Successor is given the value of the right child of the node to be deleted 
+
+            while successor.left: # While successor has a left child
+                parent = successor # parent is given the value of the successor which after the next line means it is successor's parent node
+                successor = successor.left # By doing this successor is given the value of the most bottom left node
+
+            # replace node data (the value to be deleted) by its successor data
             node.data = successor.data
+
             # fix successor's parent's child
-            if parent.left == successor:
-                parent.left = successor.right
+            if parent.left == successor: # If the successor was the value of the parent's left child node
+                parent.left = successor.right # assigns parent's left child node, the value of successor's right (parent's right) in other words, swpas the two children nodes
             else:
                 parent.right = successor.right
 
     def print_tree(self):
     
-        # Print tree content inorder
+        # Print tree content in order
 
         if self.left:
             self.left.print_tree()
@@ -192,7 +202,7 @@ class Node:
 
         return cnt
 
-root = Node(8)
+root = Node(8) # This creates a tree with a single node, 8
 root.insert(3)
 root.insert(10)
 root.insert(1)
