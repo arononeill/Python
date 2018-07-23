259231d8c70d4e591797d15b92d398f975abcba3
arononeill <arononeill98@gmail.com>
2018-07-23 15:12:53 +0100

    Implementation of a Stack

--- Data_Structures/stack.py
+++ Data_Structures/stack.py
@@ -18,16 +18,15 @@ class Stack:
          return len(self.items)
 
 
-s = Stack()
-print(s.isEmpty())
-s.push(4)
-s.push('dog')
-print(s.peek())
-s.push(True)
-print(s.size())
-print(s.isEmpty())
-s.push(8.4)
-print(s.pop())
-print(s.pop())
-print(s.size())
-print s
+s = Stack() # Creates a new Stack called s
+print(s.isEmpty()) # Checks whether stack s is empty or not, returns true
+s.push(40) # Pushes element 40 onto stack
+s.push('Tiocfaidh ar la') # Pushses element 'Tiocfaidh ar la' onto the stack 
+print(s.peek()) # Returns the element at the top of the stack
+s.push('NewElement') # Pushes element 'NewElement' onto the stack
+print(s.size()) # Returns the size of the stack
+print(s.isEmpty()) # Checks whether stack s is empty or not, returns true
+s.push(99) # Pushes element 99 onto the stack
+print(s.pop()) # Removes the top element from the stack
+print(s.pop()) # Removes the top element from the stack
+print(s.size()) # Returns the size of stack s
