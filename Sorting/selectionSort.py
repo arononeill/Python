2b2e226ffd45d92d021cf131714de8c232db3c20
arononeill <arononeill98@gmail.com>
2018-07-31 12:37:46 +0100

    Understanding Selection Sort

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -1,14 +1,15 @@
 def selectionSort(list):
-   for fillslot in range(len(list)-1,0,-1):
-       positionOfMax=0
+   for fillslot in range(length-1,0,-1):
+       GreatestPosition = 0
        for location in range(1,fillslot+1):
-           if list[location]>list[positionOfMax]:
-               positionOfMax = location
+           if list[location]>list[GreatestPosition]:
+               GreatestPosition = location
 
        temp = list[fillslot]
-       list[fillslot] = list[positionOfMax]
-       list[positionOfMax] = temp
+       list[fillslot] = list[GreatestPosition]
+       list[GreatestPosition] = temp
 
 list = [54,26,93,17,77,31,44,55,20]
+length = len(list) 
 selectionSort(list)
 print(list) 
